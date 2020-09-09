35083bfb0e3bd8a721b723107b1bf329c1493934
vagrant <vagrant@localhost.localdomain>
2020-09-09 09:59:57 +0000

    finish_task6

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
+  
+  attachment :profile_image, destroy: false
+
+  # バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
+  validates :name, length: { maximum: 20, minimum: 2 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+end
